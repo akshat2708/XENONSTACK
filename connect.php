3211289cbc157afe23408d7ed48b89bc0e94e353
akshat2708 <91825177+akshat2708@users.noreply.github.com>
2022-11-10 08:39:17 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$number = $_POST['number'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$pincode = $_POST['pincode'];
+
+//Database  Connection
+
+?>
